027d8cb2b9bbc0e551897807bcd07817938904af
Haqq Bassit <munagahmnh@gmail.com>
2023-12-31 21:49:16 +0000

    Build UI For Game Added buttons for playing the game to improve user experience

--- script.js
+++ script.js
@@ -1,52 +1,66 @@
+let computerScore = userScore = 0;
+let computerSelection, playerSelection, roundWinner;
+
+const displayScore = document.querySelector('.display-score');
+const rock = document.querySelector('.rock');
+const paper = document.querySelector('.paper');
+const scissors = document.querySelector('.scissors');
+
+
+rock.addEventListener('click', () => handleClick('rock'));
+paper.addEventListener('click', () => handleClick('paper'));
+scissors.addEventListener('click', () => handleClick('scissors'));
+
+const gameScore = document.createElement('div');
+const roundStatus = document.createElement('div');
+displayScore.appendChild(roundStatus);
+displayScore.appendChild(gameScore);
+
+initializeUI();
+
+
 function getComputerChoice() {
     let num = Math.floor(Math.random() * 3) + 1;
-    
-    return (num === 1) ? "rock" : (num === 2) ? "paper" : (num === 3) ? "scissors" : null;
+    return (num === 1) ? "rock" : (num === 2) ? "paper" : "scissors";
 }
 
 function singleRound(playerSelection, computerSelection) {
-
     if ((playerSelection === "rock" && computerSelection === "scissors") ||
         (playerSelection === "paper" && computerSelection === "rock") ||
         (playerSelection === "scissors" && computerSelection === "paper")) {
-            console.log(`You win! ${playerSelection} beats ${computerSelection}`);
+            roundStatus.innerText = `You win! ${playerSelection} beats ${computerSelection}`;
             return 'Player';
     } else if (playerSelection === computerSelection) {
-        console.log('Tie')
+        roundStatus.innerText = 'Tie';
         return 0;
     } else {
-        console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
+        roundStatus.innerText = `You lose! ${computerSelection} beats ${playerSelection}`;
         return 'Computer';
     } 
 }
 
-function game() {
-    let roundWinner;
-    for (let i = 0; i < 5; i++) {
-        console.log(`Round ${i+1}`);
-
-        playerSelection = prompt("Rock, paper, scissors").toLowerCase();
-        while (playerSelection !== "rock" && playerSelection !== "paper" && playerSelection !== "scissors") {
-            playerSelection = prompt("Invalid Input! Choose: Rock, paper, scissors").toLowerCase();
-        }
-        computerSelection = getComputerChoice();
-
-        roundWinner = singleRound(playerSelection, computerSelection);
-        if (roundWinner === 'Player') {
-            userScore++;
-        } else if (roundWinner === 'Computer') {
-            computerScore++;
-        } else {
-            i--;
-        }
-    }
+function initializeUI() {
+    computerScore = userScore = 0;
+    gameScore.textContent = `Score:: User: ${userScore} | Computer: ${computerScore}`;
 }
 
-let computerScore = userScore = 0;
-let computerSelection, playerSelection;
+function updateUI() {
+    if (userScore === 5) roundStatus.textContent = 'YOU WIN!'; 
+    else if (computerScore === 5) roundStatus.textContent = 'COMPUTER WINS!';
 
-game()
+    if (userScore === 5 || computerScore === 5) initializeUI();
 
-console.log(`FINAL SCORE: User:${userScore}, Computer:${computerScore}`)
-if (computerScore > userScore) {console.log('YOU LOSE');}
-else {console.log('YOU WIN');}
+    gameScore.textContent = `Score:: User: ${userScore} | Computer: ${computerScore}`;
+}
+
+function handleClick(choice) {
+    computerSelection = getComputerChoice()
+    playerSelection = choice;
+
+    roundWinner = singleRound(playerSelection, computerSelection);
+
+    if (roundWinner === 'Player') userScore++;
+    else if (roundWinner === 'Computer') computerScore++;
+
+    updateUI();
+}
